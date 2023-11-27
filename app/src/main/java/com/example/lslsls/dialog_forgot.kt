550c8842d632bf740113e13e9172a7fc8d149c30
Muthu0103 <arrownvig2001@gmail.com>
2023-11-27 10:47:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lslsls/dialog_forgot.kt
@@ -0,0 +1,11 @@
+package com.example.lslsls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dialog_forgot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog_forgot)
+    }
+}
